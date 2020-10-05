7c75eb494fe2f1a6dce1a037ae493b329b47f3d0
ShakyaSaman <samanshakya01@gmail.com>
2020-10-05 23:03:56 +0545

    Updates

---
+++ precache-manifest.71358a55d749d58b1ea0f58374041b24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9410bcb869a2e3f8583f11e98571846f",
+    "url": "/redux-with-api/index.html"
+  },
+  {
+    "revision": "302280f264dc200e4dfd",
+    "url": "/redux-with-api/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8c27243251dbc2f0c79b",
+    "url": "/redux-with-api/static/js/2.ff3a9d76.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/redux-with-api/static/js/2.ff3a9d76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "302280f264dc200e4dfd",
+    "url": "/redux-with-api/static/js/main.0d10220a.chunk.js"
+  },
+  {
+    "revision": "6094cd0cc1a50c22ab12",
+    "url": "/redux-with-api/static/js/runtime-main.2de49625.js"
+  }
+]);
